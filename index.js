402f1ed16d2e2ae4f931e2cb79dc75120e91bcec
nuts-rice <red44443@gmail.com>
2021-11-10 09:59:59 -0500

    basic events and config file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const { Client, Intents}m = require("discord.js");
+const { Client, Intents} = require("discord.js");
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
+const config = require("./config.json");
 
 client.on("ready", () => {
     console.log("Kick the tires, light the fires. I am ready")
 });
 
 client.on("messageCreate", (message) => {
-    if (message.content.startsWith("ping")) {
+    if (!message.content.startsWith (config.prefix) || message.author.bot) return;
+    if (message.content.startsWith('${config.prefix}ping')) {
         message.channel.send("pong!");
-    }
+    } else
+
+        if (message.content.startsWith('${confitg.prefix}minesweeper')){
+            message.channel.send("Let's play minesweeper!")
+        }
 })
 
-client.login("BOTTOKEN!");
+client.login(config.token);
